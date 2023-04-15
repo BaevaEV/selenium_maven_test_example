fa662277c3af80b9fadd3ee63aa41533aefe28d1
BaevaEV <130212730+BaevaEV@users.noreply.github.com>
2023-04-15 21:44:41 +0300

    add homework for lesson-6 new packages and files

---
+++ src/main/code/code1/FirstProgram.java
@@ -0,0 +1,9 @@
+package code.code1;
+import code.code2.TwoClass;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Любой текст");
+        TwoClass two = new TwoClass();
+    }
+}
---
+++ src/main/code/code2/TwoClass.java
@@ -0,0 +1,7 @@
+package code.code2;
+
+public class TwoClass {
+    public static void main(String[] args){
+        System.out.println("Конкретный текст");
+    }
+}
