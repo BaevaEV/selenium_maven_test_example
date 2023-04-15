7ff17588b91c8ceb2ed6600c397904c5bf5c01ea
BaevaEV <130212730+BaevaEV@users.noreply.github.com>
2023-04-16 00:21:20 +0300

    add more fixes for lesson-6 homework

--- src/main/code/code2/TwoClass.java
+++ src/main/code/code2/TwoClass.java
@@ -1,14 +1,8 @@
 package code.code2;
 
 public class TwoClass {
-    public static void main(String[] args) {
-        say();
-    }
-
-    public static boolean say() {
-        String s = "Коллеги";
-        String g = ",хорошего дня!";
-        System.out.println(s + g);
-        return true;
+    public static String say() {
+        String s = "Коллеги,хорошего дня!";
+        return s;
     }
 }
