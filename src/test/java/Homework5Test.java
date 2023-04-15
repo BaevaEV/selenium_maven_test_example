e0c92a664f0f79ad500be25dd9339d16507e5551
BaevaEV <130212730+BaevaEV@users.noreply.github.com>
2023-04-15 20:31:38 +0300

    add homework for lesson-5

---
+++ src/test/java/Homework5Test.java
@@ -0,0 +1,77 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.junit.jupiter.api.Assertions;
+
+import java.time.Duration;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
+@DisplayName(value = "Утверждения")
+public class Homework5Test {
+    WebDriver driver;
+
+    @BeforeEach
+    public void initDriver() {
+        driver = new ChromeDriver();
+    }
+
+    @Order(1)
+    @Test
+    @DisplayName(value = "One")
+    public void assert1Test() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("http://172.24.120.5:8081/login");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.findElement(By.id("login-input")).sendKeys("BAEVA");
+        driver.findElement(By.id("password-input")).sendKeys("Start123");
+        driver.findElement(By.id("form_auth_button")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='Card_containerNew__adAai']"))).click();
+        driver.findElement(By.id("note-modal-title-new_empty")).sendKeys("О весне");
+        String titleText = driver.findElement(By.id("note-modal-title-new_empty")).getText();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(text(),'Содержание')]"))).click();
+        driver.findElement(By.id("note-modal-content-new_empty")).sendKeys("Весна, весна! Как воздух чист!");
+        driver.findElement(By.id("palette-btn-new_empty")).click();
+        driver.findElement(By.id("palette-color-#fff475")).click();
+        driver.findElement(By.id("note-modal-save-btn-new_empty")).click();
+        Assertions.assertEquals("О весне",titleText, "Верный заголовок заметки");
+    }
+
+    @Order(2)
+    @Test
+    @DisplayName(value = "Two")
+    public void assert12est() {
+        driver.get("http://172.24.120.5:8081/login");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.findElement(By.id("login-input")).sendKeys("BAEVA");
+        driver.findElement(By.id("password-input")).sendKeys("Start123");
+        driver.findElement(By.id("form_auth_button")).click();;
+        driver.findElement(By.xpath("//div[contains(text(),'Весна, весна! Как воздух чист!')]")).click();
+        String contentText = driver.findElement(By.xpath("//div[contains(text(),'Весна, весна! Как воздух чист!')]")).getText();
+        Assertions.assertEquals("Весна, весна! Как воздух чист!",contentText, "Верный текст заметки");
+    }
+
+    @Order(3)
+    @Test
+    @DisplayName(value = "Three")
+    public void assert3Test() {
+        driver.get("http://172.24.120.5:8081/login");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.findElement(By.id("login-input")).sendKeys("BAEVA");
+        driver.findElement(By.id("password-input")).sendKeys("Start123");
+        driver.findElement(By.id("form_auth_button")).click();;
+        driver.findElement(By.xpath("//div[contains(text(),'Весна, весна! Как воздух чист!')]")).click();
+        String noteColor = driver.findElement(By.xpath("//div[@class='Card_container__YjYSI']")).getCssValue("background-color");
+        Assertions.assertEquals("rgba(255, 244, 117, 1)",noteColor, "да, желтый!");
+    }
+
+    @AfterEach
+    public void closeDriver() {
+        driver.quit();
+    }
+}
