d66b1e754945a607c12f459c569745a75ca9ae25
BaevaEV <130212730+BaevaEV@users.noreply.github.com>
2023-04-22 21:55:26 +0300

    add Homework7

---
+++ src/main/Homework7/Homework7.java
@@ -0,0 +1,16 @@
+package Homework7;
+
+public class Homework7 {
+    public static void main(String[] args) {
+        π();
+        x();
+    }
+        private static void π() {
+            final float π = 3.1415926f;
+            System.out.println("Значение числа π равно" + " " + π);
+        }
+        private static void x() {
+            int x = 5;
+            System.out.println("Искомое число равно" + " " + x);
+        }
+    }
