16d64c4c4c7d7b763dddc0e4d8ba846f9a5a8e58
BaevaEV <130212730+BaevaEV@users.noreply.github.com>
2023-04-30 00:34:40 +0300

    add changes for Homework10

--- src/test/java/Homework10Test.java
+++ src/test/java/Homework10Test.java
@@ -5,22 +5,20 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
-import javax.swing.*;
 import java.awt.*;
+import java.sql.SQLOutput;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 
 @DisplayName(value = "Циклы")
 public class Homework10Test {
     WebDriver driver;
+
 
 
     @BeforeEach
@@ -29,29 +27,31 @@ public class Homework10Test {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
+    @Order(1)
     @Test
-    @DisplayName(value = "create Note")
-    public void assert1Test() {
+    @DisplayName(value = "Part 1")
+    public void for1Test() {
         driver.get("http://172.24.120.5:8081/login");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         driver.findElement(By.id("login-input")).sendKeys("BAEVA");
         driver.findElement(By.id("password-input")).sendKeys("Start123");
         driver.findElement(By.id("form_auth_button")).click();
         for (int i = 0; i < 3; i++) {
             driver.findElement(By.xpath("//div[@class='Card_containerNew__adAai']")).click();
-            driver.findElement(By.id("note-modal-title-new_empty")).sendKeys("О весне"+ i);
+            driver.findElement(By.id("note-modal-title-new_empty")).sendKeys("О весне"+i);
             driver.findElement(By.xpath("//div[contains(text(),'Содержание')]")).click();
             driver.findElement(By.id("note-modal-content-new_empty")).sendKeys("Весна, весна! Как воздух чист!");
             driver.findElement(By.id("palette-btn-new_empty")).click();
             driver.findElement(By.id("palette-color-#fff475")).click();
             driver.findElement(By.id("note-modal-save-btn-new_empty")).click();
-            WebElement title = new WebDriverWait(driver, Duration.ofSeconds(20))
-                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(@style,'display')]/div["+(i+1)+"]")));
+            WebElement title = new WebDriverWait(driver, Duration.ofSeconds(30))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(@style,'display')]/div[" + (i + 1) + "]")));
             String s = title.getText();
             System.out.println(s);
-            }
-
+        }
+        int f;
             for (int j = 0; j < 3; j++) {
-                driver.findElement(By.xpath("//p[contains(text(),'О весне')]/ancestor::div[@class = 'Card_container__YjYSI']/descendant::div[@class='Card_fullscreenBtnBar__hP-57']/img[@class='img-fluid']")).click();
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(@style,'display')]/div["+(j+1)+"]/div[2]/img[1]"))).click();
                 driver.findElement(By.xpath("//div[@class='ModalCard_cardBodyInput__ghZU0 modal-title h4']")).clear();
                 driver.findElement(By.xpath("//div[@class='ModalCard_cardBodyInput__ghZU0 modal-title h4']")).sendKeys("О зиме"+j);
                 driver.findElement(By.xpath("//div[@class='ModalCard_cardBodyInput__ghZU0']")).clear();
@@ -63,7 +63,58 @@ public class Homework10Test {
                 System.out.println(k);
             }
 
+    }
+
+    @Order(2)
+    @Test
+    @DisplayName(value = "Part 2")
+    public void for2Test() {
+        driver.get("http://172.24.120.5:8081/login");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.findElement(By.id("login-input")).sendKeys("BAEVA");
+        driver.findElement(By.id("password-input")).sendKeys("Start123");
+        driver.findElement(By.id("form_auth_button")).click();
+        if (countNote() == 0) {
+            driver.findElement(By.xpath("//div[@class='Card_containerNew__adAai']")).click();
+            driver.findElement(By.id("note-modal-title-new_empty")).sendKeys("Про осень!");
+            driver.findElement(By.xpath("//div[contains(text(),'Содержание')]")).click();
+            driver.findElement(By.id("note-modal-content-new_empty")).sendKeys("Осень наступила, высохли цветы!");
+            driver.findElement(By.id("palette-btn-new_empty")).click();
+            driver.findElement(By.id("palette-color-#fff475")).click();
+            driver.findElement(By.id("note-modal-save-btn-new_empty")).click();
+
         }
+        for (int j = 1; countNote() >= j; j++) {
+            WebElement title1 = new WebDriverWait(driver, Duration.ofSeconds(20))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(@style,'display')]/div["+(j+0)+"]")));
+            String b = title1.getText();
+            System.out.println("Iteration #" +j+ " " + b);
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(@style,'display')]/div["+j+"]/div[2]/img[1]"))).click();
+            driver.findElement(By.xpath("//div[@class='ModalCard_cardBodyInput__ghZU0 modal-title h4']")).clear();
+            driver.findElement(By.xpath("//div[@class='ModalCard_cardBodyInput__ghZU0 modal-title h4']")).sendKeys("Про лето");
+            driver.findElement(By.xpath("//div[@class='ModalCard_cardBodyInput__ghZU0']")).clear();
+            driver.findElement(By.xpath("//div[@class='ModalCard_cardBodyInput__ghZU0']")).sendKeys("Я так хочу, чтобы лето не кончалось!");
+            driver.findElement(By.xpath("//button[@class='ModalCard_cardButton__g2ndB btn btn-primary']")).click();
+            WebElement title = new WebDriverWait(driver, Duration.ofSeconds(20))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(@style,'display')]/div[" + (j) + "]")));
+            String k = title.getText();
+            System.out.println("Iteration #" +j+ " " + k);
+        }
+    }
+
+    public int countNote() {
+        List<WebElement> notes = driver.findElements(By.xpath("//div[contains(@id,'note-container')]"));
+        return notes.size();
+    }
+
+    public String textFromNote() {
+        List<WebElement> textNotes = driver.findElements(By.tagName("p"));
+        for (WebElement text : textNotes)
+        return text.getText();
+        return null;
+    }
+
+
     @AfterEach
     public void closeDriver() {
         driver.quit();
