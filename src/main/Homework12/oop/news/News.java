6e7e00e2132828c381000078f9ec727acbb43bae
BaevaEV <130212730+BaevaEV@users.noreply.github.com>
2023-05-06 23:38:59 +0300

    add right waits at Homework12

---
+++ src/main/Homework12/oop/news/News.java
@@ -0,0 +1,12 @@
+package Homework12.oop.news;
+
+public class News {
+    protected void getNews(){
+        //Вывод последний новостей//
+    }
+}
+
+
+
+
+
---
+++ src/main/oop/news1/getNews.java
@@ -0,0 +1,10 @@
+package oop.news1;
+import Homework12.oop.news.News;
+
+
+public class getNews extends News {
+    public void getNews(){
+        getNews();
+        //Вывод последний новостей//
+    }
+}
